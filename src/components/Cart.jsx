6070bb34bcb6d444a813291f9f1064d2c004b693
Guilherme Rocha <guirocha1938@gmail.com>
2024-09-17 13:59:01 -0300

    feat: Adds cart item component for list items inside the cart and sets usability to add/remove items with increase/decrease buttons.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { currencyFormatter } from '../util/formatting';
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 const Cart = () => {
     const cartCtx = useContext(CartContext);
@@ -20,7 +21,14 @@ const Cart = () => {
             <h2>Your cart:</h2>
             <ul>
                 {cartCtx.items.map((item) => 
-                    <li key={item.id}>{item.name} - {item.quantity}</li>
+                    <CartItem
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        price={item.price}
+                        onIncrease={() => cartCtx.addItem(item)}
+                        onDecrease={() => cartCtx.removeItem(item.id)}
+                    />
                 )}
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
